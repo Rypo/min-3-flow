c1790cc074d56b4180b4fe584b13d2c8a520e29c
Rypo <ryan@gust.dev>
2022-08-21 02:41:01 -0500

    Add setup.py package metadata

--- setup.py
+++ setup.py
@@ -2,5 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'min3flow',
+    version='0.1.0',
+    author='Rypo',
     packages = find_packages(),
+    url='https://github.com/Rypo/min-3-flow',
+    license='MIT',
 )
